537b4d620362c50233abac7f81e4cf10e419927e
mattndross <mattndross@users.noreply.github.com>
2021-06-29 20:42:48 +0200

    excercise E done

---
+++ week-1/InClass/exercise-E.js
@@ -0,0 +1,10 @@
+const firstName = "Matías Andrés";
+const lastName = "Rossi";
+let greeting = `Hello, my name is ${firstName} ${lastName}`;
+
+console.log(greeting);
+console.log(`${firstName} ${lastName}`);
+console.log(firstName + " " + lastName);
+
+let name = "nombre en inglés";
+console.log(name);
