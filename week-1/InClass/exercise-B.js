ce1d8c0d2ef9c2517e7e3bbf0dbd4face197c58b
mattndross <mattndross@users.noreply.github.com>
2021-06-29 19:32:23 +0200

    exercise C done

--- week-1/InClass/exercise-B.js
+++ week-1/InClass/exercise-B.js
@@ -2,4 +2,4 @@ console.log("Hola Mundo!"); //español
 console.log("Ciao Mundo!"); //italiano
 console.log('Bonjour Le Monde!'); //français
 console.log("Hola món!"); // catalá
-console.log('Ola Mundo!'); //português
+console.log('Ola Mundo!'); //português co
---
+++ week-1/InClass/exercise-C.js
@@ -0,0 +1,5 @@
+const greetings = "How you doing?";
+
+console.log(greetings);
+console.log(greetings);
+console.log(greetings);
