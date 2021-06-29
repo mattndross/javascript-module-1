57f9b120132c76e93c0f1be7876bfeb0c3745b7b
mattndross <mattndross@users.noreply.github.com>
2021-06-29 23:51:14 +0200

    exercise G done

---
+++ week-1/InClass/exercise-G.js
@@ -0,0 +1,13 @@
+const numberOfStudents = 13;
+const numberOfMentors = 2;
+const totalPeople = numberOfMentors + numberOfStudents;
+
+const percentageOfStudents = Math.round((100 * numberOfStudents) / totalPeople);
+const percentageOfMentors = Math.round((100 * numberOfMentors) / totalPeople);
+
+
+
+
+
+console.log(`Percentage of students: ${percentageOfStudents}`);
+console.log(`percentage of mentors: ${percentageOfMentors}`);
