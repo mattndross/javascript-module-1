9b4fd4ac362d8c5337b3785d8acef7bcb18e779f
mattndross <mattndross@users.noreply.github.com>
2021-06-29 19:07:51 +0200

    exercise B done

---
+++ week-1/InClass/exercise-B.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo!"); //español
+console.log("Ciao Mundo!"); //italiano
+console.log('Bonjour Le Monde!'); //français
+console.log("Hola món!"); // catalá
+console.log('Ola Mundo!'); //português
