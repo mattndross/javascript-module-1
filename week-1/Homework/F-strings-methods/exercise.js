0fbd2a04a95918f35dc00661e08a47a83a78b518
mattndross <mattndross@users.noreply.github.com>
2021-06-30 10:05:12 +0200

    String methods exercises done

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
 
+const firstName = "Matías";
+let message = `Hello! My name is ${firstName} and it is ${firstName.length} characters long`;
 console.log(message);
--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
 const name = " Daniel  ";
-
+const trimmedName = name.trim();
+let message = `Hello! My name is ${trimmedName} and it is ${trimmedName.length} characters long`;
 console.log(message);
