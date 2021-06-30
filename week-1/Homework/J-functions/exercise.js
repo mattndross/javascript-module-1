c4781f943a66f4653fc249436e5c2e0439016a55
mattndross <mattndross@users.noreply.github.com>
2021-06-30 10:15:28 +0200

    J functions exercises done

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,16 @@
 function halve(number) {
   // complete the function here
+  return number / 2;
 }
 
 var result = halve(12);
+console.log(result);
 
+var result = halve(2847438);
+console.log(result);
+
+var result = halve(56);
+console.log(result);
+
+var result = halve(2);
 console.log(result);
