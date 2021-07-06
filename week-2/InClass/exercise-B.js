09f25be192d423388aed17a47c6491cc77421408
mattndross <mattndross@users.noreply.github.com>
2021-07-06 19:28:17 +0200

    excercise B done

--- week-2/InClass/exercise-B.js
+++ week-2/InClass/exercise-B.js
@@ -1,5 +1,5 @@
 function boolChecker(bool) {
-  if (typeof bool === ) {
+  if (typeof bool === 'boolean') {
     return "You've given me a bool, thanks!";
   }
 
@@ -7,3 +7,9 @@ function boolChecker(bool) {
 }
 
 boolChecker(true);
+
+console.log(boolChecker(true));
+console.log(boolChecker("string"));
+console.log(boolChecker(19));
+console.log(boolChecker(19 == 19));
+
