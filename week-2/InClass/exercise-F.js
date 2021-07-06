ca3873a08029f96c2f91e528c0bcf5c410b78cd2
mattndross <mattndross@users.noreply.github.com>
2021-07-06 20:51:47 +0200

    excercise F  done

---
+++ week-2/InClass/exercise-F.js
@@ -0,0 +1,8 @@
+const apolloCountdownMessage = "all engine running… LIFT-OFF!";
+let countdown = 8;
+
+while (countdown >= 0) {
+    console.log(countdown)
+    countdown -= 1;    
+}
+console.log(apolloCountdownMessage);
