1c805f06ff5b7e38e5a97f980464dec10269d807
mattndross <mattndross@users.noreply.github.com>
2021-07-06 21:31:35 +0200

    excercise H done

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -5,3 +5,24 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+for (let i = 5; i <= 20; i++) {
+
+  const even = isEven(i);
+  const expo = exponential(i);
+  if (even) {
+    console.log(`The exponential number of ${even} is ${expo}`);
+  }
+}
+
+
+let num = 5;
+
+while (num <= 20) {
+  const even = isEven(num);
+  const expo = exponential(num);
+  if (even){
+    console.log(`The exponential number of ${even} is ${expo}`)
+  }
+  num++;
+}
