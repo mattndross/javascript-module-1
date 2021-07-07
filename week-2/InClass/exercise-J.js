6c9178c5a4884c009dd4e434bcc899b4620d29cf
mattndross <mattndross@users.noreply.github.com>
2021-07-07 11:29:56 +0200

    exercise J done

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,15 @@
 function secondMatchesAmy(array) {
-  if ( ) {
+  if (array[1] === 'Amy') {
     return "Second index matched!";
   }
   return "Second index not matched";
 }
+
+let names = ["Alex", "Amara", "Carlos"];
+let names2 = ["Ali", "Amy", "Naresh"];
+
+const result = secondMatchesAmy(names);
+console.log(result);
+
+const result2 = secondMatchesAmy(names2);
+console.log(result2);
