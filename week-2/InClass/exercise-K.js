fef1292eb2c351262adc398b968feb7c2302a3d0
mattndross <mattndross@users.noreply.github.com>
2021-07-07 11:41:37 +0200

    exercise K done

---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,9 @@
+function printStudents(array) {
+    for (i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+const students = ['Camilo', 'Ana', 'Jorge', 'Tamara', 'Pepe'];
+
+console.log(printStudents(students));
